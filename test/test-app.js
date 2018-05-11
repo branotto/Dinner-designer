edb2c6f1b318f463326fc250e54eeb75cccccd3b
Brandon Otto <branaotto@gmail.com>
2018-05-10 22:03:38 -0400

    initial test set up, CI with travis

---
+++ test/test-app.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require ('../app');
+
+const expect = chai. expect
+
+chai.use(chaiHttp);
+
+describe('Dinner Designer', function()
+{
+
+    before(function()
+    {
+        return runServer();
+    });
+
+    after(function()
+    {
+        return closeServer();
+    });
+
+    //test strategy
+
+    it('should should return html for the root', function()
+    {
+        return chai.request(app)
+        .get('/')
+        .then(function(res)
+        {
+            expect(res).to.have.status(200);
+            expect(res).to.be.html;
+        });
+    });
+});

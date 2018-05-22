960e2d5104b822a2992f5873c229c70c06395916
Brandon Otto <branaotto@gmail.com>
2018-05-21 23:49:40 -0400

    added new recipe form, replaced recipe div with lis, started implementing edit, delete, save buttons for recipes

--- public/javascript/dinnerplans.js
+++ public/javascript/dinnerplans.js
@@ -56,29 +56,136 @@ const MOCK_RECIPE_DATA = {
         }]
     };
 
+function editRecipe()
+{
+    let $recipes = $('#recipePreferences');
+
+    $recipes.on('click', '.editRecipe', ()=>
+    {
+        let $li = $(this).closest('li');
+
+        $li.find('input.name').val( $li.find('label.name').html() );
+        $li.find('input.frequency').val( $li.find('label.frequency').html() );
+        $li.find('input.day').val( $li.find('label.day').html() );
+        $li.addClass('edit');
+       
+    });
+
+    $recipes.on('click', '.cancelEdit', ()=>
+    {
+        let $li = $(this).closest('li');
+        $li.removeClass('edit');
+       
+    });
+}
+
+function deleteRecipe()
+{
+    let $recipes = $('#recipePreferences');
+
+    $recipes.on('click', '#delete', ()=>
+    {
+        console.log("Delete Pressed!");
+      
+        let $li = $(this).parent();
+
+        //remove when API is implemented
+        $li.remove();
+
+    /* Implement AJAX delete to API
+
+    $.ajax({
+        type : "DELETE",
+        url : `/recipes/$(this).attr('data-recipe-id')`,
+        succes : () => 
+        {
+           $li.remove(); 
+        },
+        error ; () =>
+        {
+            console.log("Error deleteing recipe.");
+        };
+    });
+
+    */
+        
+    });
+}
+
+function addNewRecipe()
+{
+    let $name = $('#newRecipeName');
+    let $frequency = $('#newRecipeFrequency');
+    let $day = $('#newRecipeDay');
+
+    $('#newRecipe').on('click', (event) =>
+    {
+        event.preventDefault();
+        let recipe = 
+        {
+            name : $name.val(),
+            frequency : $frequency.val(),
+            day : $day.val()
+        };
+
+        //replace with AJAX when API is ready
+        displayRecipe(recipe);
+
+        /* use when API is ready
+        $.ajax({
+            type : 'POST',
+            url : '/recipes',
+            data : recipe,
+            succes : (newRecipe) =>
+                {
+                    displayRecipe(newRecipe);
+                },
+            error : () =>
+                {
+                    console.log('Error saving recipe.');
+                }
+        })
+        */
+    })
+}
+
+function displayRecipe(recipe) {
+    let newRecipe = `
+    <li>
+        <p>Name:
+            <label class="noEdit name" for="name">${recipe.name} 
+                <input id="name" type="text" class="edit name">
+            </label>
+        </p>
+        
+        <p>Frequency:
+            <label class="noEdit frequency" for="frequency"> ${recipe.frequency}
+                <input id="frequency" type="text" class="edit frequency">
+            <label>
+        </p>
+        
+        <p>Day Preference:
+            <label class="noEdit day" for="day">${recipe.day}
+                <input id="day" type="text" class="edit day">
+            </label>
+        </p>
+        <button class="saveEdit edit">Save</button>
+        <button class="cancelEdit edit">Cancel</button>  
+        <button class="editRecipe noEdit">Edit</button> 
+        <button data-id="${recipe.id}" id="delete">Delete</button>
+    </li>`;
+
+    $('#recipePreferences').append(newRecipe);
+}
+
 //display the recipe preferences returned from the 
 //database
 function displayRecipePreferences(data)
 {
-    let preferences = `<div>`;
-
     for (index in data.recipePreferences)
     {
-        preferences += `
-            <div>
-                <h2>${data.recipePreferences[index].name}</h2>
-                <label class="hidden" for="name" placeholder="${data.recipePreferences[index].name}> <input id="name" type="text" class="hidden">
-                <p>Recipe Frequency: ${data.recipePreferences[index].frequency}</p>
-                <label class="hidden" for="frequency" placeholder="${data.recipePreferences[index].frequency}"> <input id="frequency" type="text" class="hidden">
-                <p>Day Preference: ${data.recipePreferences[index].day}</p>
-                <label class="hidden" for="day" placeholder="${data.recipePreferences[index].day}"> <input id="day" type="text" class="hidden">
-                <button data-recipe-id="${data.recipePreferences[index].id}" class="hidden">Save</button> <button>Edit</button> <button>Delete</button>
-            </div>`;
+        displayRecipe(data.recipePreferences[index]);
     }
-
-    preferences += `</div>`;
-
-    $('#recipePreferences').append(preferences);
 
     //handle recipe button clicks()
 }
@@ -97,13 +204,27 @@ function requestAndDisplayRecipePreferences()
     getRecipePreferences(displayRecipePreferences);
 }   
 
+function handleRecipes()
+{
+    requestAndDisplayRecipePreferences();
+
+    addNewRecipe();
+
+    deleteRecipe();
+
+    editRecipe();
+
+}
+
 //when the page load request meal history and 
 //recipe preferences
 function pageLoad()
 {
+    handleRecipes();
     
-    requestAndDisplayRecipePreferences();
     //requestAndDisplayPriorMeals();
+
+    
 }
 
 (pageLoad());
